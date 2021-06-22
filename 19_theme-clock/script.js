b22e5f141a320db138bcf7beda47341017225912
Judith Feltl <judith.feltl@live.at>
2021-06-22 19:11:10 +0200

    added js to get clock working

--- 19_theme-clock/script.js
+++ 19_theme-clock/script.js
@@ -28,7 +28,17 @@ function setTime() {
     const minutes = time.getMinutes();
     const seconds = time.getSeconds();
 
-    hourEl.style.transform = `translate(-50%, -100%) rotate()`;
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`;
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`;
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`;
+}
+
+
+// map a range of numbers to another range of numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max-out_min) / (in_max - in_min) + out_min;
 }
 
 setTime();
+
+setInterval(setTime, 1000);
