a1c6ded0d352744322c02b195f6265a6f172e5ae
Judith Feltl <judith.feltl@live.at>
2021-06-23 14:21:20 +0200

    added click effect

--- 20_button-ripple-effect/script.js
+++ 20_button-ripple-effect/script.js
@@ -1,7 +1,7 @@
 const buttons = document.querySelectorAll('.ripple');
 
 buttons.forEach(button => {
-    button.addEventListener('click', (e) => {
+    button.addEventListener('click', function (e) {
         const x = e.clientX;
         const y = e.clientY;
 
@@ -11,6 +11,13 @@ buttons.forEach(button => {
         const xInside = x - buttonLeft;
         const yInside = y - buttonTop;
 
-        
+        const circle = document.createElement('span');
+        circle.classList.add('circle');
+        circle.style.top = yInside + 'px';
+        circle.style.left = xInside + 'px';
+
+        this.appendChild(circle); // 'this' does not work with arrow-functions
+
+        setTimeout(() => circle.remove(), 500);
     })
 })
