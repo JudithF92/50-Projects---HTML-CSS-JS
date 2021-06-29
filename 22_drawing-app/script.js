a470db378a5c49736d638c187e24c88c226e64fc
Judith Feltl <judith.feltl@live.at>
2021-06-29 20:11:01 +0200

    adding drawLine function

--- 22_drawing-app/script.js
+++ 22_drawing-app/script.js
@@ -3,6 +3,8 @@ const ctx = canvas.getContext('2d');
 
 let size = 20;
 let color = 'black';
+let x;
+let y;
 
 function drawCircle(x, y) {
     ctx.beginPath();
@@ -11,4 +13,14 @@ function drawCircle(x, y) {
     ctx.fill();
 }
 
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size;
+    ctx.stroke();
+}
+
 drawCircle(100, 200);
+drawLine(300, 300, 200, 500);
